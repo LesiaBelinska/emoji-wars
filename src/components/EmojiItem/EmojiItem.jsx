2f0b0e7f13f96cf981e67fa13fe9e73b75283596
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-01 13:28:01 +0200

    Update EmojiItem.jsx

--- src/components/EmojiItem/EmojiItem.jsx
+++ src/components/EmojiItem/EmojiItem.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import s from "./EmojiItem.module.css";
 
-const EmojiItem = ({ emoji, votes }) => {
-    return (
-        <div className={ s.item }>
-            <span className={ s.img }>{ emoji }</span>
-            <span className={ s.votes }>{ votes }</span>
-        </div>
-    )
-}
+const EmojiItem = ({ emoji, votes, onClick, hidden }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={` ${s.item} ${hidden ? s.hidden : ""}`}
+    >
+      <span className={s.emoji}>{emoji}</span>
+      <span className={s.votes}>votes: {votes}</span>
+    </div>
+  );
+};
 
 export default EmojiItem;

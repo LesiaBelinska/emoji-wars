5c2dbdbf84ee64d127cfd98b092a5a8d15fb8b3a
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-01 13:27:51 +0200

    Update PrimaryButton.jsx

--- src/components/Buttons/PrimaryButton/PrimaryButton.jsx
+++ src/components/Buttons/PrimaryButton/PrimaryButton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import s from "./PrimaryButton.module.css";
 
-const PrimaryButton = ({buttonText}) => {
-    return (
-      <button type="button" className={s.button}>
-        {buttonText}
-      </button>
-    );
-}
+const PrimaryButton = ({ onClick, buttonText }) => {
+  return (
+    <button onClick={onClick} type="button" className={s.button}>
+      {buttonText}
+    </button>
+  );
+};
 
 export default PrimaryButton;
